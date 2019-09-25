f5de83370bbc0c030904c4332a921e16ac8debe4
Benjamin Paepke <cph-bp133@cphbusiness.dk>
2019-09-25 11:57:37 +0200

    Added small function with a UnitTest as well

--- src/main/java/core/Quotes.java
+++ src/main/java/core/Quotes.java
@@ -1,5 +1,9 @@
 package core;
 public class Quotes {
-    public Quotes() {
+    int number;
+    public Quotes(int number) {
+        this.number = number;
     }
+    
+    public int getNumber() {return number;}
 }
